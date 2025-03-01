331e2e61569e1791eabe2dae22bcbeaf41915b51
Eli Rudman <erudman21@gmail.com>
2025-02-28 21:20:36 -0800

    add db init file back

--- scripts/smoke_test.sh
+++ scripts/smoke_test.sh
@@ -2,21 +2,21 @@
 set -e
 
 # Test frontend
-if ! curl --silent --fail --max-time 10 http://$EC2_IP:80; then
+if ! curl --silent --fail --max-time 10 http://localhost:80; then
     echo "Frontend failed"
     exit 1
 fi
 echo "Frontend works"
 
 # Test backend
-if ! curl --silent --fail --max-time 10 http://$EC2_IP:9080/users; then
+if ! curl --silent --fail --max-time 10 http://localhost:9080/users; then
     echo "Backend failed"
     exit 1
 fi
 echo "Backend works"
 
 # Test basic db stuff
-create_response=$(curl -s -X POST -H "Content-Type: application/json" -d '{"firstName":"Test","lastName":"User","email":"test@example.com"}' http://$EC2_IP:9080/users)
+create_response=$(curl -s -X POST -H "Content-Type: application/json" -d '{"firstName":"Test","lastName":"User","email":"test@example.com"}' http://localhost:9080/users)
 if [[ "$create_response" != *"New user was created"* ]]; then
     echo "DB failed"
     exit 1
