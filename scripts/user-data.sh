e8f33f0e2cf65fed19dcb1eb7a1580e520c46a97
Eli Rudman <erudman21@gmail.com>
2025-02-28 19:45:18 -0800

    remove ec2 termination

--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -51,16 +51,6 @@ volumes:
 
 EOL_COMPOSE
 
-cat > database/init.sql << 'EOL_SQL'
-use userdb;
-
-CREATE TABLE `users` (`id` bigint(20) UNSIGNED NOT NULL, `first_name` varchar(200) NOT NULL, `last_name` varchar(200) NOT NULL, `email` varchar(200) DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-ALTER TABLE `users` ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `id` (`id`);
-
-ALTER TABLE `users` MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-EOL_SQL
-
 # Login to ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${ECR_REGISTRY}
 
