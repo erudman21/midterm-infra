a4d8868dee867e531145397f6dccd91f412257a2
Eli Rudman <erudman21@gmail.com>
2025-03-02 10:49:39 -0800

    remove terminate

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -41,6 +41,6 @@ else
 fi
 
 # Terminate the instance
-TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
-aws ec2 terminate-instances --instance-ids $INSTANCE_ID --region us-east-1
+# TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+# INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
+# aws ec2 terminate-instances --instance-ids $INSTANCE_ID --region us-east-1
